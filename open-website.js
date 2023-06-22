5d5a2b8cd2e878988065ec290202257a5e2e440a
Tadifuncctioun <110220188+Tadifuncctioun@users.noreply.github.com>
2023-06-22 14:34:26 +0800

    Add files via upload

---
+++ open-website.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const headerLinks = document.getElementsByClassName('header-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    headerLinks.classList.toggle('active')
+})
